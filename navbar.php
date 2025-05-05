d1958262d7a8295d5a235fe4befc15cb2f9ae5ee
Phtevn98 <stevenwhitelaw98@gmail.com>
2025-05-05 21:58:47 +0100

    Adding bootstrap stylesheet.

--- navbar.php
+++ navbar.php
@@ -1,5 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php"> Chores App <i class="fa-solid fa-hands-bubbles"></i> </a>
-    <a href="add_chores.php"> Add Chores </a> |
-    <a href="about.php"> About </a>
+    <a class="navbar-brand" href="index.php">
+        Chores App <i class="fa-solid fa-hands-bubbles"></i>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="add_chores.php">Add Chores</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+            </li>
+        </ul>
+    </div>
 </nav>
