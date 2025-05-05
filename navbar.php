cda766c2e1f5ec31a88cb46a9681c334f573fc5d
Phtevn98 <stevenwhitelaw98@gmail.com>
2025-05-05 15:10:26 +0100

    Adding bootstrap stylesheet.

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
-<nav style="background: #ffffff; padding: 1px;">
-    <a href="index.php"> Home </a> |
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php"> Chores App <i class="fa-solid fa-hands-bubbles"></i> </a>
     <a href="add_chores.php"> Add Chores </a> |
     <a href="about.php"> About </a>
 </nav>
