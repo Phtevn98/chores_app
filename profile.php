ae8c4697984487d5a88a53cce948de1a54d8fb6b
Phtevn98 <stevenwhitelaw98@gmail.com>
2025-05-11 14:48:31 +0100

    Adding session login checks to every page, refining login logic, added some functionality to profile page.

---
+++ profile.php
@@ -0,0 +1,33 @@
+<?php
+include 'header.php';
+
+if (!isset($_SESSION['user_id'], $_SESSION['username'])) {
+    header('Location: login.php');
+    exit;
+}
+
+$username = htmlspecialchars($_SESSION['username']);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $username; ?>'s Profile</title>
+    <?php include 'header.php'; ?>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+
+<div class="container mt-4 text-center">
+    <h1><?php echo $username; ?>'s Profile</h1>
+
+    <img src="https://www.svgrepo.com/show/335455/profile-default.svg"
+         alt="Profile picture"
+         class="rounded-circle mb-3"
+         width="120"
+         height="120">
+
+    <p><strong>Username:</strong> <?php echo $username; ?></p>
+</div>
+</body>
+</html>
