9ed6ad17b2f9bb140f2efe2ee5d5051a90dafe56
Phtevn98 <stevenwhitelaw98@gmail.com>
2025-05-16 22:26:44 +0100

    Hide navbar items if user isn't logged in.

--- navbar.php
+++ navbar.php
@@ -4,31 +4,33 @@
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"> </span>
+        <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="add_chores.php">Add Chores</a>
-            </li>
-        </ul>
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="add_chores.php">Add Chores</a>
+                </li>
+            </ul>
 
-        <!-- Profile Dropdown -->
-        <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdown"
-                   role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="https://www.svgrepo.com/show/335455/profile-default.svg" alt="Profile"
-                         width="30" height="30" class="rounded-circle me-2">
-                    My Profile
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="profile.php">View Profile</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                </ul>
-            </li>
-        </ul>
+            <!-- Profile Dropdown -->
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdown"
+                       role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://www.svgrepo.com/show/335455/profile-default.svg" alt="Profile"
+                             width="30" height="30" class="rounded-circle me-2">
+                        My Profile
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="profile.php">View Profile</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        <?php endif; ?>
     </div>
 </nav>
