a047f717a6c66be30caa6da005b04addd87b89e3
Phtevn98 <stevenwhitelaw98@gmail.com>
2025-05-16 22:35:31 +0100

    Add in some extra functionality to the chore cards, added stylesheet.

--- index.php
+++ index.php
@@ -24,13 +24,29 @@ $chores_results = pg_query($dbconnect, "SELECT * FROM chores");
 <body>
 <?php include 'navbar.php'; ?>
 
-<div class="container mt-4">
-    <h1>Chores List</h1>
-    <ul>
+<div class="chore-cards-container">
+    <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
         <?php while ($chore = pg_fetch_assoc($chores_results)): ?>
-            <li><?php echo htmlspecialchars($chore['chore_name']); ?></li>
+            <div class="col">
+                <div class="card h-100 shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo htmlspecialchars($chore['chore_name']); ?></h5>
+                        <?php if (!empty($chore['description'])): ?>
+                            <p class="card-text"><?php echo htmlspecialchars($chore['description']); ?></p>
+                        <?php endif; ?>
+                        <?php if (!empty($chore['assigned_to'])): ?>
+                            <span class="badge bg-primary">Assigned to: <?php echo htmlspecialchars($chore['assigned_to']); ?></span>
+                        <?php endif; ?>
+                    </div>
+                    <div class="card-footer text-muted">
+                        <?php if (!empty($chore['due_date'])): ?>
+                            Due: <?php echo htmlspecialchars(date('j M Y', strtotime($chore['due_date']))); ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
         <?php endwhile; ?>
-    </ul>
+    </div>
 </div>
 <?php
 include 'footer.php';
